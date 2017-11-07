97f6630924c93d052e41c75a9db60197d13d09c6
KapowFreshGitHub <33102390+KapowFreshGitHub@users.noreply.github.com>
2017-11-07 15:39:02 +0000

    Details, details

---
+++ AS Programming - Part 1/Details, details/Details, details.py
@@ -0,0 +1,12 @@
+#details details
+#declare firstName : STRING
+#declare surname : STRING
+#declare age : INTEGER
+#declare PT : STRING
+firstName = input("Enter your first name")
+surname = input("Enter your surname")
+age = int(input("Enter your age"))
+PT = input("Enter your PT")
+print(firstName, surname)
+print("Age: ",age)
+print("PT is: ",PT)
