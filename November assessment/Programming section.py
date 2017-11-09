60c15347db32cade839d28f71fbfa54e8fddfa76
KapowFreshGitHub <33102390+KapowFreshGitHub@users.noreply.github.com>
2017-11-09 13:37:17 +0000

    November assessment

---
+++ November assessment/Programming section.py
@@ -0,0 +1,22 @@
+#Nicole Carrera #AS computing class 1 01/11/17
+
+#declare counter = INTEGER
+#declare Teamname = STRING
+#declare Playerhits = INTEGER
+#declare Totalhits = INTEGER
+#declare Averagehits = REAL
+#declare PointsEarned = INTEGER
+
+Counter = 0
+Teamname ="Zeus"
+Playerhits = 0
+Totalhits = 0
+Averagehits = 0.0
+Pointsearned = 0
+
+Teamname= input("Enter team name")
+Playerhits = int(input("Enter number of hits"))
+Totalhits = Totalhits + Playerhits
+for counter in range(6,0,-1)
+    print(counter)
+
