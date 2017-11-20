60aeebf3567b79193cece4a541e107a93e3a0f91
KapowFreshGitHub <33102390+KapowFreshGitHub@users.noreply.github.com>
2017-11-20 09:34:30 +0000

    ARRAY TASK(2)

---
+++ TASKS/1D ARRAYS TASK/1D ARRAY TASK (Q2).py
@@ -0,0 +1,10 @@
+#DECLARE myPetName: ARRAY (0-3) OF STRING
+myPetName = ["","","",""]
+
+myPetName[0] = "Lucky"
+myPetName[1] = "Leonie"
+myPetName[2] = "Socks"
+myPetName[3] = "Bonnie"
+
+for index in range (0, 4):
+    print( myPetName [index])
