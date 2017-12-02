1aad8996468323a87ee961c3510ef5e7bab9124d
KapowFreshGitHub <33102390+KapowFreshGitHub@users.noreply.github.com>
2017-12-02 19:17:05 +0000

    THIS WORKS BUT COULD YOU DOUBLE CHECK PLZ

---
+++ TASKS/SELECTION (III) TASK/Q1/(Q1) 1.6.py
@@ -0,0 +1,9 @@
+#DECLARE ValidCoin: STRING
+
+print("Input Coin")
+ValidCoin = input()
+
+if ValidCoin == "10" or ValidCoin == "20" or ValidCoin == "50" or ValidCoin == "100":
+    print("TRUE")
+else:
+    print("FALSE")
