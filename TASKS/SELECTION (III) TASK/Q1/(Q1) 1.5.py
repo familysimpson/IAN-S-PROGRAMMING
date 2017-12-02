3e75167cba2645973c89acbff9c40de691633246
KapowFreshGitHub <33102390+KapowFreshGitHub@users.noreply.github.com>
2017-12-02 18:55:34 +0000

    THERE IS AN ERROR AND I DONT KNOW HOW TO FIX IT CAN YOU PLEASE CHECK THIS THX

---
+++ TASKS/SELECTION (III) TASK/Q1/(Q1) 1.5.py
@@ -0,0 +1,11 @@
+#DECLARE TEMPERATURE: INTEGER
+
+print("Enter Temperature")
+Temperature = int(input())
+
+if Temperature > 25:
+    print("Hot")
+if Temperature >= 20 and = 25:
+    print("Just Right")
+else:
+    print("Cold")
