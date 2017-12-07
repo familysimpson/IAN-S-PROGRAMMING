850b6586c62184bfca583717adeb752d517656d1
KapowFreshGitHub <33102390+KapowFreshGitHub@users.noreply.github.com>
2017-12-07 14:22:03 +0000

    JANUARY ASSESSMENT PREPARATION

--- Programming Challenge/January Assessment preparation/January Assessment preparation.py
+++ Programming Challenge/January Assessment preparation/January Assessment preparation.py
@@ -2,6 +2,7 @@
 
 print("Enter the four names of the team")
 Four_names_of_teams = ["","","",""]
+
 Four_names_of_teams[0] = input("Enter team 1")
 Four_names_of_teams[1] = input("Enter team 2")
 Four_names_of_teams[2] = input("Enter team 3")
@@ -14,7 +15,7 @@ print(Four_names_of_team1)
 print(Four_names_of_team2)
 
 Answer1= random.randint(1,10)
-print("Enter a number")
+print(Four_names_of_team1,"Enter a number")
 number = int(input())
 while number != Answer1:
         print("Enter another number")
@@ -25,7 +26,7 @@ Answer2 = random.randint(1,10)
 print("Enter a number")
 number = int(input())
 while number != Answer2:
-        print("Enter another number")
+        print(Four_names_of_team2,"Enter another number")
         number = int(input())
 print("Correct number!")
 
