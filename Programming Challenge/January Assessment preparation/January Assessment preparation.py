db5d48ef9d77521a82fa531463e10ceb4eb5732e
KapowFreshGitHub <33102390+KapowFreshGitHub@users.noreply.github.com>
2017-12-07 13:42:12 +0000

    JANUARY ASSESSMENT PREPARATION

---
+++ Programming Challenge/January Assessment preparation/January Assessment preparation.py
@@ -0,0 +1,18 @@
+#JANUARY ASSESSMENT PREPARATION  #GUESSINGGAME #NICOLE_CARRERA #07/12/17
+
+print("Enter the four names of the team")
+Four_names_of_teams = ["","","",""]
+
+Four_names_of_teams[0] = ""
+Four_names_of_teams[1] = ""
+Four_names_of_teams[2] = ""
+Four_names_of_teams[3] = ""
+
+import random
+Four_names_of_teams = random.randrange(Four_names_of_teams)
+
+Four_names_of_teams = random.randint(1,10)
+
+
+
+
