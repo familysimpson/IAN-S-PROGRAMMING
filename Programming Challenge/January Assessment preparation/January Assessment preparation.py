aee403f4aa9e6f48485838940fda3ecf6ce1d59c
KapowFreshGitHub <33102390+KapowFreshGitHub@users.noreply.github.com>
2017-12-07 14:12:54 +0000

    JANUARY ASSESSMENT PREPARATION

--- Programming Challenge/January Assessment preparation/January Assessment preparation.py
+++ Programming Challenge/January Assessment preparation/January Assessment preparation.py
@@ -2,22 +2,31 @@
 
 print("Enter the four names of the team")
 Four_names_of_teams = ["","","",""]
-
-Four_names_of_teams[0] = ""
-Four_names_of_teams[1] = ""
-Four_names_of_teams[2] = ""
-Four_names_of_teams[3] = ""
+Four_names_of_teams[0] = input("Enter team 1")
+Four_names_of_teams[1] = input("Enter team 2")
+Four_names_of_teams[2] = input("Enter team 3")
+Four_names_of_teams[3] = input("Enter team 4")
 
 import random
-Four_names_of_teams = random.randrange(Four_names_of_teams)
+Four_names_of_team1 = random.choice(Four_names_of_teams)
+Four_names_of_team2 = random.choice(Four_names_of_teams)
+print(Four_names_of_team1)
+print(Four_names_of_team2)
 
-Four_names_of_teams[] = random.randint(1,10)
-Four_names_of_teams[] = random.randint(1,10)
-    print("Enter a number")
-    number = int(input())
-    while number != 7:
+Answer1= random.randint(1,10)
+print("Enter a number")
+number = int(input())
+while number != Answer1:
         print("Enter another number")
-    number = int(input())
-        print("Correct number!")
+        number = int(input())
+print("Correct number!")
+
+Answer2 = random.randint(1,10)
+print("Enter a number")
+number = int(input())
+while number != Answer2:
+        print("Enter another number")
+        number = int(input())
+print("Correct number!")
 
 
