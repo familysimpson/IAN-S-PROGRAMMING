701096f48bf95af11803e9adb7866f2b99341eb9
KapowFreshGitHub <33102390+KapowFreshGitHub@users.noreply.github.com>
2017-12-19 20:55:52 +0000

    Rounding numbers - Functions

--- Programming Challenge/January Assessment preparation/January Assessment preparation.py
+++ Programming Challenge/January Assessment preparation/January Assessment preparation.py
@@ -16,20 +16,46 @@ Four_names_of_team2 = random.choice(Four_names_of_teams)
 print(Four_names_of_team1)
 print(Four_names_of_team2)
 
+noAttempts1 = 0
+guess1 = -1
+
+noAttempts2 = 0
+guess2 = -1
+
 Answer1 = random.randint(1,10)
-print(Four_names_of_team1,"Enter a number")
+print(Four_names_of_team1,"Enter a number from 1 to 10")
 number = int(input())
 while number != Answer1:
         print("Enter another number")
         number = int(input())
+        noAttempts1 += 1
 print("Correct number!")
+print("Player 1 ;here is your number of attempts taken:", noAttempts1)
 
 Answer2 = random.randint(1,10)
-print("Enter a number")
+print(Four_names_of_team2,"Enter a number from 1 to 10")
 number = int(input())
 while number != Answer2:
         print(Four_names_of_team2,"Enter another number")
         number = int(input())
+        noAttempts2 += 1
 print("Correct number!")
+print("Player 2; here is your number of attempts taken:",noAttempts2)
+
+if noAttempts1 > noAttempts2:
+        print("Player 1 is the winner!")
+elif noAttempts2 > noAttempts1:
+        print("Player 2 is the Winner!")
+elif noAttempts1== noAttempts2:
+        print("It's a draw!")
 
 
+REPEAT1 = print(Four_names_of_team1,"Would you like to play again? YES OR NO")
+while REPEAT1 != "quit":
+        Four_names_of_team1.clear()
+        noAttempts1.clear()
+
+REPEAT2 = print(Four_names_of_team2,"Would you like to play again? YES OR NO")
+while REPEAT1 != "quit":
+        Four_names_of_team2.clear()
+        noAttempts1.clear()
