a80fd8f7db4c2627ba0d03b83e0cca19aaaebb39
KapowFreshGitHub <33102390+KapowFreshGitHub@users.noreply.github.com>
2018-01-18 13:26:40 +0000

    FILE HANDLING

---
+++ TASKS/2D ARRAY TASK/FruityArray.py
@@ -0,0 +1,9 @@
+rownumber = int()
+input("Enter the row number, ", rownumber)
+while rownumber > 0 or < 4:
+    print("Incorrect. Please enter a number from 0 to 4")
+
+columnnumber = int()
+input("Enter the column number, ", columnnumber)
+while columnnumber > 3 or < 0 :
+    print("Incorrect. Please enter a number from 0 to 3")
