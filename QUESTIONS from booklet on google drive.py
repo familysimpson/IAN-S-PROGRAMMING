32ad381f8d8b394f5decf9873329c8b702f62069
KapowFreshGitHub <33102390+KapowFreshGitHub@users.noreply.github.com>
2018-02-08 11:33:00 +0000

    BOOKLET

---
+++ QUESTIONS from booklet on google drive.py
@@ -0,0 +1,25 @@
+"""QUESTION
+A computer program is used to store a patient’s heart rate each day for a week.
+The seven readings are stored in an array of real numbers called “bpm”.
+Using Pseudocode or a programming language of your choice,
+write a short program to calculate the average heart rate of the patient over the seven days.
+"""
+
+#DECLARE bpm: ARRAY[0:7] OF REAL
+#DECLARE days: INTEGER
+bpm = [float,float,float,float,float,float,float]
+
+bpm [0] = 80.1
+bpm [1] = 80.1
+bpm [2] = 80.1
+bpm [3] = 80.1
+bpm [4] = 80.1
+bpm [5] = 80.1
+bpm [6] = 80.1
+
+days = 7
+
+sum = sum(bpm[0:7])
+avg_hrt_rate = sum / days
+print(avg_hrt_rate)
+
