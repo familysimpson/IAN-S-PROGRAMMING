61e339c52c03c4768a4c8e7f68cc9fd4af24803c
KapowFreshGitHub <33102390+KapowFreshGitHub@users.noreply.github.com>
2018-02-09 10:15:47 +0000

    PRIME NUMBERS

---
+++ PAUL'S TASKS/Prime numbers.py
@@ -0,0 +1,13 @@
+#NC_T4_PRIMES
+
+"""User inputs a number (e.g 20 or 2000)
+Program should print on screen all prime numbers less than or equal to that number
+(e.g 2,3,,5,7,11,13,17,19)
+"""
+
+number = int(input("Enter a number"))
+
+if number <= number
+    
+
+
