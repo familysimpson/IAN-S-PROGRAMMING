180d7e27c88e110be4917c9be93b7c0b9e0ce76d
KapowFreshGitHub <33102390+KapowFreshGitHub@users.noreply.github.com>
2018-02-12 11:45:22 +0000

    PRIME NUMBERS

--- PAUL'S TASKS/Prime numbers.py
+++ PAUL'S TASKS/Prime numbers.py
@@ -2,12 +2,23 @@
 
 """User inputs a number (e.g 20 or 2000)
 Program should print on screen all prime numbers less than or equal to that number
-(e.g 2,3,,5,7,11,13,17,19)
+(e.g 2,3,5,7,11,13,17,19)
 """
 
-number = int(input("Enter a number"))
+#DECLARE VarA : INTEGER
+#DECLARE x: INTEGER
 
-if number <= number
-    
+max = int(input("Find primes up to what number: "))
+
+for x in range(2,max + 1):
+    isPrime = True
+    for y in range(2 , x):
+        if x % y == 0:
+            isPrime = False
+    if isPrime:
+        print(x)
+
+
+
 
 
