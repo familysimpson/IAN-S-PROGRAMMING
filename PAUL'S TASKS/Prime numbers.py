b5ef156d4e35dad747b28f4a4e7e108bfe208680
KapowFreshGitHub <33102390+KapowFreshGitHub@users.noreply.github.com>
2018-02-21 11:05:53 +0000

    PAUL'S PROGRAMMING

--- PAUL'S TASKS/Prime numbers.py
+++ PAUL'S TASKS/Prime numbers.py
@@ -5,16 +5,29 @@ Program should print on screen all prime numbers less than or equal to that numb
 (e.g 2,3,5,7,11,13,17,19)
 """
 
-#DECLARE debbie_input : INTEGER
-#DECLARE x: INTEGER
+#DECLARE input_num : INTEGER
+#DECLARE isPrime: BOOLEAN
+#DECLARE counter: INTEGER
 #DECLARE y: INTEGER
 
-debbie_input = int(input("Find primes up to what number: "))
+input_num = int(input("Find primes up to what number: "))
 
-for x in range(2, debbie_input + 1):
+for counter in range(2, input_num + 1):
+#the counter is between the number 2 and between the number the user inputs e.g 2 to 5 (remember python does 1 number
+#less so you have to add one)
     isPrime = True
-    for y in range(2 , x):
-        if x % y == 0:
+#you create a variable isPrime which is a boolean and you set it to be true at first
+    for y in range(2 ,counter):
+        #again here you create a for loop within a for loop as this time you want to find the prime numbers up
+        #until the numbers within the counter (e.g 2 to 5) so below is where it does the working out of each number
+        # and seeing if it is a prime or not
+        if counter % y == 0:
+            #if the number has a remainder of 0 then it's not a prime number
+            # when divided by a number in y e.g 15/2 has a remainder of 1 next y
+            # is 3 so 15/3 has a remainder of 0
             isPrime = False
-    if isPrime:
-        print(x)
+            #if there isn't a remainder of 0 when dividing by each numbers leading up to the number inputted by user
+            # then it will be still be set to true
+            #otherwise isPrime will be set to False
+    if isPrime == True:
+        print(counter)
