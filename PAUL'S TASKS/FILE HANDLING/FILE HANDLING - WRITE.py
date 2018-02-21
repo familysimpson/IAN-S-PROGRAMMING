ba26520003dda78ce12fcb43e07856839c3aede4
KapowFreshGitHub <33102390+KapowFreshGitHub@users.noreply.github.com>
2018-02-21 10:31:05 +0000

    FILE HANDLING

--- PAUL'S TASKS/FILE HANDLING/FILE HANDLING - WRITE.py
+++ PAUL'S TASKS/FILE HANDLING/FILE HANDLING - WRITE.py
@@ -4,8 +4,6 @@ Create text file called temp.txt.py
 Input a string from the keyboard and write it into the file
 Close the file
 
-
-
 """
 LineOfText = input("Hello world, Enter your name")
 FileHandle = open("temp.txt.py","w")
