a3919e9cfce03c936f44e942feaa956c10b78966
KapowFreshGitHub <33102390+KapowFreshGitHub@users.noreply.github.com>
2018-02-25 19:41:18 +0000

    RESTAURANT ORDER

--- Random practice/Restraunt menu orders.py
+++ Random practice/Restraunt menu orders.py
@@ -8,16 +8,36 @@ SetMenuOption[3] = "Fries"
 SetMenuOption[4] = "Rice and brocolli"
 SetMenuOption[5] = "Peach and rice"
 
-order = [int,int,int,int,int,int]
+customer_order = [int,int,int,int,int,int]
 
-order[0] = int(input())
-order[1] = int(input())
-order[2] = int(input())
-order[3] = int(input())
-order[4] = int(input())
-order[5] = int(input())
+customer_order[0] = int
+customer_order[1] = int
+customer_order[2] = int
+customer_order[3] = int
+customer_order[4] = int
+customer_order[5] = int
 
-nocustomers = int(input("How many customers are ordering at one table?"))
-print(nocustomers)
+Total_customers = 0
 
-print("How many customers want",SetMenuOption[0])
+no_customers = int(input("How many customers are ordering at one table?"))
+print(no_customers,"customers")
+
+for counter in range(0,6):
+
+    print("How many customers ordered:",SetMenuOption[counter])
+    customer_order[counter] = int(input("Input number of customers:"))
+
+    Total_customers = Total_customers + customer_order[counter]
+    print("Total orders so far:",Total_customers)
+
+    if Total_customers == no_customers:
+        print("Thanks for your order")
+        break
+    if Total_customers >= no_customers:
+        print("Too many orders. Start again.")
+        break
+    else:
+        print(customer_order[counter], "orders :", SetMenuOption[counter])
+
+if Total_customers < no_customers:
+    print("Too little orders. Start again.")
