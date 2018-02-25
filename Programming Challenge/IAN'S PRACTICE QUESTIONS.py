8e3d786728c91b92e0aaf9c9a0c09f332e545dbe
KapowFreshGitHub <33102390+KapowFreshGitHub@users.noreply.github.com>
2018-02-25 16:42:38 +0000

    MOCK EXAM REGIMER - PRACTICE QUESTION (IAN)

--- Programming Challenge/IAN'S PRACTICE QUESTIONS.py
+++ Programming Challenge/IAN'S PRACTICE QUESTIONS.py
@@ -1,15 +1,22 @@
 
 day = 0
 noExams = 0
+day_name = ["","","","","","",""]
+
+day_name[0] = "Monday"
+day_name[1] = "Tuesday"
+day_name[2] = "Wednesday"
+day_name[3] = "Thursday"
+day_name[4] = "Friday"
+day_name[5] = "Monday"
+day_name[6] = "Tuesday"
 
 for counter in range (0,7):
-    examday = input("Do you have an exam on this day?")
-    if examday == "Yes" or "yes" or "Y":
-         noExams = noExams + 1
-    else:
-        noExams = noExams + 0
-print("You have",noExams, "exams. Goodluck!")
-
+    print("Please answer with 'Yes/y/Y/yes' if you have an exam on",day_name[counter])
+    examday = str(input())
+    if examday == "Yes" or examday == "Y" or examday == "yes" or examday == "y":
+        noExams = noExams + 1
+print("You have",noExams, "exam/s. Goodluck!")
 
 
 
