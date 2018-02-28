5acbf983480a41eb3f3409099cc71a56f7e9d94d
KapowFreshGitHub <33102390+KapowFreshGitHub@users.noreply.github.com>
2018-02-28 11:14:22 +0000

    100 YEARS OLD

---
+++ Random practice/100 YEARS OLD.py
@@ -0,0 +1,12 @@
+"""Create a program that asks the user their name and their age.
+Print out a message addressed to them that tell them the year that they will turn 100 years old """
+
+#DECLARE name: STRING
+#DECLARE age: INTEGER
+#DECLARE future_age: INTEGER
+
+name = str(input("Please enter your name"))
+age = int(input("Please enter your age"))
+
+future_years = 100 - age
+print(name,"you will be 100 in",future_years, "years")
