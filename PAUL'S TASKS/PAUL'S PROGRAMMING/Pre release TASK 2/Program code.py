1ddb7e8f3bf942895e3e44669483779d311946cc
KapowFreshGitHub <33102390+KapowFreshGitHub@users.noreply.github.com>
2018-03-17 15:43:06 +0000

    Program code

---
+++ PAUL'S TASKS/PAUL'S PROGRAMMING/Pre release TASK 2/Program code.py
@@ -0,0 +1,11 @@
+def addNewMember(name, memberID, category):
+    print
+
+def printMembershipCard(name, memberID, category):
+    print
+
+def calculateTotalMembers(membersJ, membersS, membersSTD, noJuniors, noSeniors, noStandards):
+    print
+
+def deleteMember(memberID, membersJ, membersS, membersSTD):
+    print
