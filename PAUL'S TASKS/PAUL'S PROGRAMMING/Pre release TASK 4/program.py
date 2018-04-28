464ce8e1349295998f6db44171377e0ec57cc811
KapowFreshGitHub <33102390+KapowFreshGitHub@users.noreply.github.com>
2018-04-28 13:39:02 +0100

    PRE-RELEASE 4 - TASK HELP PLZ

---
+++ PAUL'S TASKS/PAUL'S PROGRAMMING/Pre release TASK 4/program.py
@@ -0,0 +1,41 @@
+
+#DECLARE femaleSalary: ARRAY [0:4] OF INTEGER
+#DECLARE maleSalary: ARRAY [0:4] OF INTEGER
+
+femaleSalary = [0,0,0,0,0]
+maleSalary = [0,0,0,0,0]
+
+#line = "nicole,F,90000.00"
+fh = open('Salary.TXT.py','r')
+line = fh.readline()
+while len(line) > 0:
+    endpos = line.find(',')
+    line= line[endpos+1:]
+    endpos = line.find(',')
+    gender = line[0:endpos]
+    salary = float(line[endpos+1:])
+    print(gender,salary)
+    line = fh.readline()
+
+    if salary >= 0 or salary <= 25000.0:
+        elementID = 0
+
+    elif salary > 25000.0 or salary <= 50000.0:
+        elementID = 1
+
+    elif salary > 50000.0 or salary <= 75000.0:
+        elementID = 2
+
+    elif salary > 75000.0 or salary <= 100000.0:
+        elementID = 3
+
+    elif salary > 100000.0 or salary <= 110000.0:
+        elementID = 4
+
+    if gender == "F":
+            femaleSalary[elementID] = femaleSalary[elementID] + 1
+    elif gender == "M":
+            maleSalary[elementID] = maleSalary[elementID] + 1
+
+print("Here is the number for the female salary" , femaleSalary)
+print("Here is the number for the male salary", maleSalary)
