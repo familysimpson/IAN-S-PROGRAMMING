5132e95d7f5b64d5eb21524c0194c0cb0e676bdf
KapowFreshGitHub <33102390+KapowFreshGitHub@users.noreply.github.com>
2018-06-20 11:44:22 +0100

    FLASHY GAME

--- Flashy Game/Answer verification.py
+++ Flashy Game/Answer verification.py
@@ -1,24 +1,29 @@
-
+#DECLARE Question: STRING
 #DECLARE AnsVerify: BOOLEAN
 #DECLARE User_Ans: STRING
 #DECLARE Answer: STRING #insert answer from file (FINN)
-Question = "Is a compiler a type of translator?"
-Answer = "A compiler is a type of translator " #This will be answer from file
+#DECLARE loopy: INTEGER # it till loop 20 times
+
+Question = "Is a compiler a type of translator?"#This will be question from file #finn
+Answer = "A compiler is a type of translator " #This will be answer from file #finn
+
 Ans_Verify = False
 Count = 0
-print(Question)
-print(Answer)
+loopy = 20
 
-User_ans = str(input("Please enter 'y' if you achieved the correct answer and please enter 'n' if you achieved the incorrect answer:"))
-while User_ans != "n" and User_ans != "y":
-    print("Please type in either 'y' or 'n'")
+for loopy in range(0,20):
+    print(Question)
+    print(Answer)
     User_ans = str(input("Please enter 'y' if you achieved the correct answer and please enter 'n' if you achieved the incorrect answer:"))
+    while User_ans != "n" and User_ans != "y":
+        print("Please type in either 'y' or 'n'")
+        User_ans = str(input("Please enter 'y' if you achieved the correct answer and please enter 'n' if you achieved the incorrect answer:"))
 
-if User_ans == "y":
-    Ans_Verify = True
-    Count = Count + 1
-    print("Well done you have 1 more point")
-elif User_ans == "n":
-    Ans_Verify = False
-    print("Don't worry just try again.")
-
+    if User_ans == "y":
+        Ans_Verify = True
+        Count = Count + 1
+        print("Well done you have 1 more point")
+        print("Here is your current count: ",Count)
+    elif User_ans == "n":
+        Ans_Verify = False
+        print("Don't worry just try again.")
