370ad10c9cb721a2697b317f375361b214ba0abc
KapowFreshGitHub <33102390+KapowFreshGitHub@users.noreply.github.com>
2018-06-20 10:43:19 +0100

    FLASHY GAME

--- Flashy Game/Answer verification.py
+++ Flashy Game/Answer verification.py
@@ -2,23 +2,23 @@
 #DECLARE AnsVerify: BOOLEAN
 #DECLARE User_Ans: STRING
 #DECLARE Answer: STRING #insert answer from file (FINN)
-
+Question = "Is a compiler a type of translator?"
 Answer = "A compiler is a type of translator " #This will be answer from file
 Ans_Verify = False
 Count = 0
+print(Question)
 print(Answer)
 
-User_ans = str(input("Please enter YES if you achieved the correct answer and please enter NO if you achieved the incorrect answer:"))
-while User_ans != "NO" and User_ans != "YES":
-    print("Please type in either YES or NO")
-    User_ans = str(input("Please enter YES if you achieved the correct answer and please enter NO if you achieved the incorrect answer:"))
+User_ans = str(input("Please enter 'y' if you achieved the correct answer and please enter 'n' if you achieved the incorrect answer:"))
+while User_ans != "n" and User_ans != "y":
+    print("Please type in either 'y' or 'n'")
+    User_ans = str(input("Please enter 'y' if you achieved the correct answer and please enter 'n' if you achieved the incorrect answer:"))
 
-if User_ans == "YES":
+if User_ans == "y":
     Ans_Verify = True
     Count = Count + 1
     print("Well done you have 1 more point")
-elif User_ans == "NO" or User_ans == "no" or User_ans == "No":
+elif User_ans == "n":
     Ans_Verify = False
     print("Don't worry just try again.")
 
-Grade
