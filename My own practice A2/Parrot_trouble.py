b2dc1fb2590167f1493c7b7bce9d41b66817143c
KapowFreshGitHub <33102390+KapowFreshGitHub@users.noreply.github.com>
2018-09-13 22:41:35 +0100

    PARROT TALKING

---
+++ My own practice A2/Parrot_trouble.py
@@ -0,0 +1,19 @@
+
+#hour = int(input("Enter hour please"))
+talking = False
+
+def parrot_trouble(hour):
+    while hour > 23 or hour <0:
+        print("Please enter values from 0 to 23")
+        hour = int(input("Enter hour again please: "))
+
+    if hour < 7 or hour > 20:
+         talking = True
+         return talking
+         #print(talking, " :parrot is talking")
+    else:
+        talking = False
+        return talking
+        #print(talking, " :parrot is not talking")
+
+print(parrot_trouble(5))
